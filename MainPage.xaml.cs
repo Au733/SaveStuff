ee403663b1a2512f5583abcf544238bb8854731d
Kou Lee <leek733@gmail.com>
2024-10-10 17:58:34 -0700

    Comments added

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,12 +12,12 @@ public partial class MainPage : ContentPage
 
     private void Save_OnClicked(object sender, EventArgs e)
     {
-        Preferences.Set("Username", txtUsername.Text);
+        Preferences.Set("Username", txtUsername.Text); // Stores username. Key = Username , Value = txtUsername.Text
     }
 
     private void Read_OnClicked(object sender, EventArgs e)
     {
-        txtUsername.Text = Preferences.Get("Username", "");
+        txtUsername.Text = Preferences.Get("Username", ""); // Retrieves username from preferences
     }
 
     private void Clear_OnClicked(object sender, EventArgs e)
